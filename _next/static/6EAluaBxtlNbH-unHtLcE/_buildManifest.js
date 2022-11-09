20504730bd9a85368777787ab6c01d893bb8b16d
kyun <wls5563@gmail.com>
2022-11-09 13:25:07 +0900

    Updates

---
+++ _next/static/6EAluaBxtlNbH-unHtLcE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/952-f3a48a10b54e6004.js","static/chunks/pages/index-448d75b35b5060f1.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
